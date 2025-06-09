7acbc24281bb4c27875b59b72bac81a806e3bf98
Anil Bharath <a.bharath@imperial.ac.uk>
2025-06-09 22:03:58 +0100

    Updated README.md

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,5 @@
-from .model import UNet1, UNet1SC
+from .model import UNet1SnC
 from .eval import *
 from .utils import *
-from .predict import Unet1Fracture
+
 #from .load_data import *
--- model/model.py
+++ model/model.py
@@ -5,37 +5,11 @@ import torch as T
 
 from .unet1parts import *
 
-class UNet1(nn.Module):
-    def __init__(self, n_channels, n_classes):
-        super(UNet1, self).__init__()
-        self.inc = inconv1(n_channels, 64)
-        self.down1 = down(64, 128)
-        self.down2 = down(128, 256)
-        self.down3 = down(256, 512)
-        self.down4 = down(512, 512)
-        self.up1 = up(1024, 256)
-        self.up2 = up(512, 128)
-        self.up3 = up(256, 64)
-        self.up4 = up(128, 64)
-        self.outc = outconv1(64, n_classes)
 
-    def forward(self, x):
-        x1 = self.inc(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
-        x = self.outc(x)
-        return T.sigmoid(x)
-
-class UNet1SC(nn.Module):
-    # Small number of channels
+class UNet1SnC(nn.Module):
+    # A simplified version of a 1D UNet architecture with a modest number of channels
     def __init__(self, n_channels, n_classes):
-        super(UNet1SC, self).__init__()
+        super(UNet1SnC, self).__init__()
         self.inc = inconv1(n_channels, 16)
         self.down1 = down(16, 32)
         self.down2 = down(32, 64)
@@ -59,49 +33,3 @@ class UNet1SC(nn.Module):
         x = self.up4(x, x1)
         x = self.outc(x)
         return T.sigmoid(x), T.argmax(x, dim=1) # NOT Differentiable
-
-# Example usage:
-# model = SimpleNet()
-# input = torch.randn(10, 256)  # Batch of 10 samples, each of length 256
-# output = model(input)
-# print(output)
-class SimpleNet(nn.Module):
-    def __init__(self):
-        super(SimpleNet, self).__init__()
-        self.fc1 = nn.Linear(256, 128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 32)
-        self.fc4 = nn.Linear(32, 16)
-        self.fc5 = nn.Linear(16, 1)
-
-    def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = torch.relu(self.fc3(x))
-        x = torch.relu(self.fc4(x))
-        x = self.fc5(x)
-        return x
-
-
-class SimpleClassy(nn.Module):
-    def __init__(self):
-        super(SimpleNet, self).__init__()
-        self.fc1 = nn.Linear(256, 128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 32)
-        self.fc4 = nn.Linear(32, 16)
-        self.fc5 = nn.Linear(16, 2)  # Output layer with 2 units for 2 classes
-
-    def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = torch.relu(self.fc3(x))
-        x = torch.relu(self.fc4(x))
-        x = self.fc5(x)
-        return torch.softmax(x, dim=1)  # Apply softmax to the output
-
-# Example usage:
-# model = SimpleNet()
-# input = torch.randn(10, 256)  # Batch of 10 samples, each of length 256
-# output = model(input)
-# print(output)
